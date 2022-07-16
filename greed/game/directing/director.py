8fcc517a8e7e53243b572a7464bf8f3dfa80f2ae
Sam Eastburn <sam@sameastburn.com>
2022-07-15 23:10:44 -0600

    first commit

---
+++ greed/game/directing/director.py
@@ -0,0 +1,90 @@
+from game.shared.point import Point
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        _keyboard_service (KeyboardService): For getting directional input.
+        _video_service (VideoService): For providing video output.
+    """
+
+    def __init__(self, keyboard_service, video_service):
+        """Constructs a new Director using the specified keyboard and video services.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._keyboard_service = keyboard_service
+        self._video_service = video_service
+        self._score = 0
+        
+    def start_game(self, cast):
+        """Starts the game using the given cast. Runs the main game loop.
+
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.open_window()
+        while self._video_service.is_window_open():
+            self._get_inputs(cast)
+            self._do_updates(cast)
+            self._do_outputs(cast)
+        self._video_service.close_window()
+
+    def _get_inputs(self, cast):
+        """Gets directional input from the keyboard and applies it to the robot.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        robot = cast.get_first_actor("robots")
+        velocity = self._keyboard_service.get_direction()
+
+        robot.set_velocity(Point(velocity.get_x(), 0))
+
+    def _do_updates(self, cast):
+        """Updates the robot's position and resolves any collisions with artifacts.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        banner = cast.get_first_actor("banners")
+        robot = cast.get_first_actor("robots")
+        artifacts = cast.get_actors("artifacts")
+
+        max_x = self._video_service.get_width()
+        max_y = self._video_service.get_height()
+        robot.move_next(max_x, max_y)
+        
+        for artifact in artifacts:
+            pos = artifact.get_position()
+            artifact.set_velocity(Point(0, 15))
+            artifact.move_next(max_x, max_y)
+
+            robot_pos = robot.get_position()
+            artifact_pos = artifact.get_position()
+
+            if abs(robot_pos.get_x() - artifact_pos.get_x()) < 10 and abs(robot_pos.get_y() - artifact_pos.get_y()) < 10:
+                message = artifact.get_message()
+                if (message == '*'):
+                  self._score = self._score + 1
+                  banner.set_text(f'Score: {self._score}')
+                  cast.remove_actor("artifacts", artifact)
+                else:
+                  self._score = self._score - 1
+                  banner.set_text(f'Score: {self._score}')
+                  cast.remove_actor("artifacts", artifact)
+        
+    def _do_outputs(self, cast):
+        """Draws the actors on the screen.
+        
+        Args:
+            cast (Cast): The cast of actors.
+        """
+        self._video_service.clear_buffer()
+        actors = cast.get_all_actors()
+        self._video_service.draw_actors(actors)
+        self._video_service.flush_buffer()
